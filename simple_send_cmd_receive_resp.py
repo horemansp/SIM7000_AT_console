0c4eca03e0037f238b110c19aa2abfb7e47a0ea1
Patrik Horemans <horemansp@hotmail.com>
2022-05-21 13:06:09 +0200

    Update simple_send_cmd_receive_resp.py

--- simple_send_cmd_receive_resp.py
+++ simple_send_cmd_receive_resp.py
@@ -2,16 +2,17 @@
 # includes +++ with delay of 1 sec before command and 1 sec after command, before sending \r\n
 # special commands:
 #    CTRL-START will restart the module (power off/on etc) = initialisation
-#    CTRL-Z will send \x1A (EOF)
-#
+#    <eof> will send \x1A (EOF)
+#    <ncr> at the end of command will prevent from sending '\r' at end of line
+#    <cr> will only send '\r' and ignore all the rest on the line
 
 import _thread  as thread
 import time
 from machine import Pin, UART
 
 # init outputs
-pwr_key = 4
-led = 12
+#pwr_key = 4
+#led = 12
 pwr_key = Pin(4, Pin.OUT)
 led_blue = Pin(12, Pin.OUT)
 
@@ -38,7 +39,7 @@ def SIM_start():
     pwr_key.on()
     time.sleep(1)
     pwr_key.off()
-    time.sleep(5)
+
     
 def get_input():
     global data 
@@ -46,28 +47,30 @@ def get_input():
         data = input()
         #print("data entered = :",data)
     
-get_input_thread = thread.start_new_thread(get_input, ())  #upython versie  lege tuple indien geen argumenten worden doorgegeven
+
 
 def write_command(data_to_send):
     if(data_to_send == "CTRL-START"):
         SIM_start()
     else:
-        if(data_to_send == "CTRL-Z"):
+        if(data_to_send == "<eof>"):
             print("write to UART: EOF")
-            uart2.write(('\x1A'+'\r\n').encode())
-        if("<n>" in data_to_send): #no CR LF at the end
-            data_to_send = data_to_send.replace("<n>","")
-            print("write to UART:",data_to_send, " without CR LF")
+            uart2.write(b'\x1A')
+        if("<ncr>" in data_to_send): #no CR at the end
+            data_to_send = data_to_send.replace("<ncr>","")
+            print("write to UART:",data_to_send, " without CR")
             uart2.write((data_to_send).encode())
-        elif ("<rn>" in data_to_send): #only CR LF
-            print("write to UART: only ACR LF")
-            uart2.write(('\r\n').encode())         
+        elif ("<cr>" in data_to_send): #only CR
+            print("write to UART: only CR")
+            uart2.write(b'\r')         
         else:
-            print("write to UART:",data_to_send," with CR LF")
+            print("write to UART:",data_to_send,"<CR> (with CR)")
             time.sleep(1) 
-            uart2.write((data_to_send).encode())
-            time.sleep(1) 
-            uart2.write(('\r\n').encode())
+            uart2.write((data_to_send + "\r").encode())
+
+
+get_input_thread = thread.start_new_thread(get_input, ())  #upython versie  lege tuple indien geen argumenten worden doorgegeven
+
 
 while 1:
     while uart2.any() > 0 :
@@ -87,7 +90,3 @@ while 1:
         #print("new data=" , data)
         write_command(data)
         data =""
-        
-            
-        
-    
